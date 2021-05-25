940a157471f387fc645aae18769ee99637ae1d79
Hyeseong-int <a01025819869@gmail.com>
2021-05-25 13:48:07 +0900

    web2-Node.js 14 to 19. impl 404 and main page

--- main.js
+++ main.js
@@ -5,38 +5,67 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.name);
-    if(_url == '/'){
-      title = "welcome"
-    }
-    if(_url == '/favicon.ico'){
-        return response.writeHead(404);
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname ==='/'){
+      if(queryData.id === undefined){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, describtion) {
+          var title = 'Welcom';
+          var describtion = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${describtion}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, describtion) {
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${describtion}</p>
+          </body>
+          </html>
+           `;
+        response.writeHead(200);
+        response.end(template);
+        });
+      }
       
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, describtion) {
-      var template = `
-    <!doctype html>
-<html>
-<head>
-  <title>WEB1 - ${title}</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-  <h2>${title}</h2>
-  <p>${describtion}</p>
-</body>
-</html>
-    `;
-    response.end(template);
-    })
-    
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
+
 });
 app.listen(3000);
--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,9 +1,14 @@
+var args = process.argv;
+console.log(args);
+console.log(args[2]);
+
 console.log('A');
 console.log('B');
-if(false) {
+
+if(args[2] == '1') {
     console.log('C1');
 }
-if(true)    {
+else {
     console.log('C2');
 }
-console.log('D');
+console.log('D');   
