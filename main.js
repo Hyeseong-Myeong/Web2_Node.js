4803f769e0134a1eb8a768c3f4269d4348c8ea0c
Hyeseong-int <a01025819869@gmail.com>
2021-05-26 14:46:26 +0900

    web2_Node.js from20 to 24. impl dynamic list

--- main.js
+++ main.js
@@ -3,38 +3,54 @@ var fs = require('fs');
 var url = require('url');
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
 
-    if(pathname ==='/'){
-      if(queryData.id === undefined){
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, describtion) {
-          var title = 'Welcom';
-          var describtion = 'Hello, Node.js';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${describtion}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        });
-      } else {
+  if(pathname ==='/'){
+    if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+        var title = 'Welcom';
+        var describtion = 'Hello, Node.js';
+      
+        var list = '<ul>';
+        var i = 0;
+        while(i < filelist.length){
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+
+          i = i + 1;
+        }
+        
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${describtion}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      })
+    } else {
+      fs.readdir('./data', function(error, filelist){
+        var title = 'Welcom';
+        var describtion = 'Hello, Node.js';
+      
+        var list = '<ul>';
+        var i = 0;
+        while(i < filelist.length){
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+
+          i = i + 1;
+        }
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, describtion) {
           var title = queryData.id;
           var template = `
@@ -46,26 +62,20 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
+            ${list}
             <h2>${title}</h2>
             <p>${describtion}</p>
           </body>
           </html>
-           `;
-        response.writeHead(200);
-        response.end(template);
+          `;
+          response.writeHead(200);
+          response.end(template);
         });
-      }
-      
+      });
+    }  
   } else {
     response.writeHead(404);
     response.end('Not found');
   }
-
-
 });
 app.listen(3000);
